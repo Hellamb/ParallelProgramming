46b3b0b3a769a1cfa65df4030f38c80948a2161c
Vladyslav-Kuksiuk <vladyslav.kuksiuk@teamdev.com>
2023-03-25 14:54:36 +0200

    Implement Task2 in Lab1.

---
+++ app/src/main/java/Lab1/Task2/Pocket.java
@@ -0,0 +1,27 @@
+package Lab1.Task2;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
+public class Pocket {
+    private final int x;
+    private final int y;
+    private final int size;
+    private final Rectangle2D rectangle;
+
+    public Pocket(int x, int y, int size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        rectangle = new Rectangle2D.Double(x, y, size, size);
+    }
+
+    public void draw(Graphics2D g2) {
+        g2.setColor(Color.GREEN);
+        g2.fill(rectangle);
+    }
+
+    public boolean isBallCollected(int x, int y, int ballRadius) {
+        return rectangle.intersects(x, y, ballRadius, ballRadius);
+    }
+}
